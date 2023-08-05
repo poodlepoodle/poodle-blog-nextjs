d4a26dd6ba64aad956e413fa7b76541ee97bebe6
poodlepoodle <chammal97@naver.com>
2023-08-06 06:05:42 +0900

    :sparkles: :: 홈 페이지 라우팅 추가

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <section className={styles.layout}>
+      <div className={styles.container}>
+        <h3>메인입니당</h3>
+      </div>
+    </section>
+  );
+}
