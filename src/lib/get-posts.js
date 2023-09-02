aebabf544ce353f3022f7757d1d86c444d006125
poodlepoodle <chammal97@naver.com>
2023-09-03 03:53:50 +0900

    :sparkles: :: add /post fetching api lib file

---
+++ src/lib/get-posts.js
@@ -0,0 +1,29 @@
+import matter from 'gray-matter';
+import path from 'path';
+import fs from 'fs/promises';
+import { cache } from 'react';
+
+export const getPosts = cache(async () => {
+  const posts = await fs.readdir('./posts');
+
+  return Promise.all(
+    posts
+      .filter((file) => path.extname(file) === '.mdx')
+      .map(async (file) => {
+        const filePath = `./posts/${file}`;
+        const postContent = await fs.readFile(filePath, 'utf8');
+        const { data, content } = matter(postContent);
+
+        if (data.published === false) {
+          return null;
+        }
+
+        return { ...data, body: content };
+      })
+  );
+});
+
+export async function getPost(slug) {
+  const posts = await getPosts();
+  return posts.find((post) => post.slug === slug);
+}
