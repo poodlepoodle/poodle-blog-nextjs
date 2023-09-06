237f7a73157d4fef8ec5a267940d89d7efe3b932
poodlepoodle <chammal97@naver.com>
2023-09-07 05:38:34 +0900

    :bug: :: fix thumbnail normal component padding bug

--- src/app/components/thumbnail/large/index.jsx
+++ src/app/components/thumbnail/large/index.jsx
@@ -5,20 +5,17 @@ import Link from 'next/link';
 
 export default function Large({ title, slug, thumbnail, date }) {
   return (
-    <div className={styles.layout}>
-      <div className={styles.container}>
-        <Link href={`/blog/${slug}`}>
+    <Link href={`/blog/${slug}`}>
+      <div className={styles.layout}>
+        <div className={styles.container}>
           <div className={styles.thumbnail__container}>
             <Image src={thumbnail} fill alt="post thumbnail large" />
           </div>
-        </Link>
 
-        <span className={styles.date}>{date}</span>
-
-        <span className={styles.title}>
-          <Link href={`/blog/${slug}`}>{title}</Link>
-        </span>
+          <span className={styles.date}>{date}</span>
+          <span className={styles.title}>{title}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
