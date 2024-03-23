397f32d6cfdc54fb8f0617cfabe4d8257f110743
poodlepoodle <chammal97@naver.com>
2024-03-23 21:10:02 +0900

    :lipstick: :: change strong styles

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Page() {
           <h1>
             새로운 기술이 파도처럼 몰려와도
             <br />
-            지워지지 않을 <strong>개발자국</strong>을 남깁니다.
+            지워지지 않을 <strong>`개발자국`</strong>을 남깁니다.
           </h1>
         </div>
       </section>
