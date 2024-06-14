b3dc713aa023cf6ff16f7b18ff5ffc0f56d2c13b
poodlepoodle <chammal97@naver.com>
2024-06-14 18:59:55 +0900

    :lipstick: :: add resizable image props to mdx-image

--- src/mdx/components/mdx-image.jsx
+++ src/mdx/components/mdx-image.jsx
@@ -3,5 +3,21 @@ import styles from './mdx-components.module.css';
 import Image from 'next/image';
 
 export default function MDXImage({ src, alt }) {
+  let imageWidth;
+  const url = new URL(src, 'https://poodlepoodle.me');
+  const widthParam = url.searchParams.get('width');
+  if (widthParam) {
+    imageWidth = parseInt(widthParam);
+    return (
+      <Image
+        src={src}
+        alt={alt}
+        width={imageWidth}
+        height={100}
+        className={styles.mdx__image}
+      />
+    );
+  }
+
   return <Image src={src} alt={alt} fill className={styles.mdx__image} />;
 }
