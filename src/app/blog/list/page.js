5c2b1880dc0998c4d1a2d462e2687724b8652b96
poodlepoodle <chammal97@naver.com>
2024-07-08 19:46:51 +0900

    :bug: :: fix `useSearchParam()` outside of `Suspence`

--- src/app/blog/list/page.js
+++ src/app/blog/list/page.js
@@ -1,9 +1,14 @@
 import { getPosts } from '@/lib/get-posts';
 import SearchPage from '@/app/components/search/page';
+import { Suspense } from 'react';
 
 export default async function Page() {
   const posts = await getPosts();
   const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-  return <SearchPage posts={sortedPosts} />;
+  return (
+    <Suspense>
+      <SearchPage posts={sortedPosts} />;
+    </Suspense>
+  );
 }
