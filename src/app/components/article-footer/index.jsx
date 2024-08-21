09e5d0f7a7de08ef75c76bd932d7b5424eb00ed4
poodlepoodle <chammal97@naver.com>
2024-08-21 19:42:03 +0900

    :recycle: :: replace articles footer section with `ArticleFooter` component

---
+++ src/app/components/article-footer/index.jsx
@@ -0,0 +1,25 @@
+import styles from './article-footer.module.css';
+
+import Image from 'next/image';
+
+export default function ArticleFooter({ children }) {
+  return (
+    <section className={styles.layout}>
+      <div className={styles.profile__container}>
+        <div className={styles.profile__info}>
+          <span>published by</span>
+          <h6>Eojin Choi</h6>
+        </div>
+        <div className={styles.profile__image}>
+          <Image
+            src={`/blog/article-profile.png`}
+            alt="article profile"
+            fill
+            style={{ objectFit: 'cover', borderRadius: '50%' }}
+          />
+        </div>
+      </div>
+      {children}
+    </section>
+  );
+}
