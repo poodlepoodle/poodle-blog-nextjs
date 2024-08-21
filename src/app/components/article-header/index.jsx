47afc9a5bccc1e7179664b4cb83c25d4fee63dc1
poodlepoodle <chammal97@naver.com>
2024-08-21 19:55:09 +0900

    :sparkles: :: put article thumbnail section into `ArticleHeader`

--- src/app/components/article-header/index.jsx
+++ src/app/components/article-header/index.jsx
@@ -1,17 +1,29 @@
 import styles from './article-header.module.css';
 
+import Image from 'next/image';
 import Tag from '@components/tag';
 
-export default function ArticleHeader({ title, date, tags }) {
+export default function ArticleHeader({ title, date, tags, slug }) {
   return (
-    <section className={styles.layout}>
-      <span className={styles.title}>{title}</span>
-      <span className={styles.date}>{date}</span>
-      <span className={styles.tags}>
-        {tags.map((tag) => (
-          <Tag key={tag} text={tag} />
-        ))}
-      </span>
-    </section>
+    <>
+      <section className={styles.info__container}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.date}>{date}</span>
+        <span className={styles.tags}>
+          {tags.map((tag) => (
+            <Tag key={tag} text={tag} />
+          ))}
+        </span>
+      </section>
+
+      <section className={styles.thumbnail__container}>
+        <Image
+          src={`/blog/posts/${slug}/thumbnail-large.jpg`}
+          alt="post thumbnail"
+          fill
+          style={{ objectFit: 'cover', borderRadius: '0.5rem' }}
+        />
+      </section>
+    </>
   );
 }
