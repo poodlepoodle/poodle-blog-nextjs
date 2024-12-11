f08ddb814ad17490866511da327db4ab620aee6d
poodlepoodle <chammal97@naver.com>
2024-12-11 21:17:52 +0900

    :sparkles: :: add about page (with temporary images)

--- src/app/components/pdf-viewer/index.jsx
+++ src/app/components/pdf-viewer/index.jsx
@@ -2,6 +2,8 @@
 
 import styles from './pdf-viewer.module.css';
 
+import Image from 'next/image';
+
 import { useState } from 'react';
 
 export default function PDFViewer() {
@@ -11,9 +13,7 @@ export default function PDFViewer() {
     setSelectedMenu(idx);
   };
 
-  const handleClickDownload = () => {
-    // alert('pdf 다운로드');
-  };
+  const handleClickDownload = () => {};
 
   return (
     <div className={styles.layout}>
@@ -40,18 +40,109 @@ export default function PDFViewer() {
             이력서
           </div>
         </div>
-        <div>
+        {/* <div>
           <div
             className={styles.download__button}
             onClick={handleClickDownload}
           >
             PDF 다운로드
           </div>
-        </div>
+        </div> */}
       </div>
+
       <div className={styles.content__container}>
-        {selectedMenu === 0 && <span>포트폴리오 내용</span>}
-        {selectedMenu === 1 && <span>이력서 내용</span>}
+        {selectedMenu === 0 && (
+          <>
+            <Image
+              src={`/portfolio/portfolio-1.webp`}
+              alt="portfolio page 1"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-2.webp`}
+              alt="portfolio page 2"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-3.webp`}
+              alt="portfolio page 3"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-4.webp`}
+              alt="portfolio page 4"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-5.webp`}
+              alt="portfolio page 5"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-6.webp`}
+              alt="portfolio page 6"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/portfolio/portfolio-7.webp`}
+              alt="portfolio page 7"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+          </>
+        )}
+        {selectedMenu === 1 && (
+          <>
+            <Image
+              src={`/resume/resume-1.webp`}
+              alt="resume page 1"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/resume/resume-2.webp`}
+              alt="resume page 2"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/resume/resume-3.webp`}
+              alt="resume page 3"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/resume/resume-4.webp`}
+              alt="resume page 4"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+            <Image
+              src={`/resume/resume-5.webp`}
+              alt="resume page 5"
+              width={960}
+              height={600}
+              style={{ objectFit: 'cover' }}
+            />
+          </>
+        )}
       </div>
     </div>
   );
