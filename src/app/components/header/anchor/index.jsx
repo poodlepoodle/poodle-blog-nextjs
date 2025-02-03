5f87993afda49cefedbe130e3eb62a0d9ae84b48
poodlepoodle <chammal97@naver.com>
2025-02-03 20:39:40 +0900

    :sparkles: :: add `HeaderAnchor` component
    needed to be use header animation

---
+++ src/app/components/header/anchor/index.jsx
@@ -0,0 +1,8 @@
+'use client';
+import useHeaderAnchor from '@hooks/useHeaderAnchor';
+
+export default function HeaderAnchor() {
+  const { headerAnchorRef } = useHeaderAnchor();
+
+  return <div ref={headerAnchorRef}></div>;
+}
