74d2f2fc016a6a405597704ce507eec4aebec044
poodlepoodle <chammal97@naver.com>
2025-02-03 21:19:45 +0900

    :dizzy: :: update background animation styles on `Article`

--- src/app/components/article/index.jsx
+++ src/app/components/article/index.jsx
@@ -5,14 +5,28 @@ import ArticleHeader from '@components/article-header';
 import ArticleFooter from '@components/article-footer';
 import Giscus from '@components/giscus';
 
+import { useBlogContext } from '@contexts/BlogContext';
+import { useEffect } from 'react';
 import useSpotlight from '@hooks/useSpotlight';
 
 export default function Article({ slug, post, children }) {
-  const { headerRef, footerRef, spotlighted } = useSpotlight();
+  const { headerRef, footerRef } = useSpotlight();
+  const { isSpotlighted, setIsSpotlighted, setHeaderText } = useBlogContext();
+
+  useEffect(() => {
+    setHeaderText(post.title);
+
+    return () => {
+      setTimeout(() => setIsSpotlighted(false), 0);
+      setHeaderText('');
+    };
+  }, []);
 
   return (
     <div
-      className={`${styles.layout} ${spotlighted ? styles.spotlighted : ''}`}
+      className={`${styles.layout} ${
+        !!isSpotlighted ? styles.dark : styles.light
+      }`}
     >
       <div className={styles.container}>
         <ArticleHeader
