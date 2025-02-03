da03ab8619d4b873b2dcc732a68f6e8a8c70c121
poodlepoodle <chammal97@naver.com>
2025-02-03 21:51:13 +0900

    :recycle: :: refactor hooks with inner `useIntersectionObserver` hook

--- src/hooks/useHeaderAnchor.js
+++ src/hooks/useHeaderAnchor.js
@@ -1,30 +1,9 @@
-import { useEffect, useRef } from 'react';
 import { useBlogContext } from '@contexts/BlogContext';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
 
 export default function useHeaderAnchor() {
-  const headerAnchorRef = useRef(null);
   const { setIsFloating } = useBlogContext();
-
-  useEffect(() => {
-    const handleIntersection = (entries) => {
-      const isIntersectingAnchor = entries.every(
-        (entry) => !entry.isIntersecting
-      );
-      setIsFloating(isIntersectingAnchor);
-    };
-
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0,
-    });
-
-    if (headerAnchorRef.current) {
-      observer.observe(headerAnchorRef.current);
-    }
-
-    return () => {
-      if (headerAnchorRef.current) observer.unobserve(headerAnchorRef.current);
-    };
-  }, []);
+  const [headerAnchorRef] = useIntersectionObserver(1, setIsFloating);
 
   return { headerAnchorRef };
 }
--- src/hooks/useSpotlight.js
+++ src/hooks/useSpotlight.js
@@ -1,35 +1,9 @@
-import { useEffect, useRef } from 'react';
 import { useBlogContext } from '@contexts/BlogContext';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
 
 export default function useSpotlight() {
-  const headerRef = useRef(null);
-  const footerRef = useRef(null);
   const { setIsSpotlighted } = useBlogContext();
-
-  useEffect(() => {
-    const handleIntersection = (entries) => {
-      const isIntersectingContent = entries.every(
-        (entry) => !entry.isIntersecting
-      );
-      setIsSpotlighted(isIntersectingContent);
-    };
-
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0,
-    });
-
-    if (headerRef.current) {
-      observer.observe(headerRef.current);
-    }
-    if (footerRef.current) {
-      observer.observe(footerRef.current);
-    }
-
-    return () => {
-      if (headerRef.current) observer.unobserve(headerRef.current);
-      if (footerRef.current) observer.unobserve(footerRef.current);
-    };
-  }, []);
+  const [headerRef, footerRef] = useIntersectionObserver(2, setIsSpotlighted);
 
   return { headerRef, footerRef };
 }
