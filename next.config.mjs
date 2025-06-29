2d7a3eff6d905ed9bbbb7005538f13b8e0155620
poodlepoodle <chammal97@naver.com>
2025-06-29 09:19:49 +0900

    :wrench: :: update config related to tailwind and MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,6 @@
-import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
-
-const withMDX = createMDX({
-  options: {
-    extension: /\.mdx?$/,
-    remarkPlugins: [, remarkGfm],
-    rehypePlugins: [],
-    // providerImportSource: '@mdx-js/react',
-  },
-});
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
-  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    };
-
-    return config;
-  },
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
