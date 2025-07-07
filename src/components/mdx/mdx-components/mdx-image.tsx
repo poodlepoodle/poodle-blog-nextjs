5dacdaad2fd0dad24a63fd02ae8c3f2a10f33956
poodlepoodle <chammal97@naver.com>
2025-07-07 18:50:55 +0900

    :sparkles: :: change MDXImage to client component, add click handler

--- src/components/mdx/mdx-components/mdx-image.tsx
+++ src/components/mdx/mdx-components/mdx-image.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import type { BlogContextType } from '@contexts/types';
+
 import styles from './mdx-components.module.css';
 import Image from 'next/image';
+import { useBlogContext } from '@contexts/BlogContext';
 
 interface MDXImageProps {
   src: string;
@@ -9,16 +14,23 @@ interface MDXImageProps {
 }
 
 export default function MDXImage({ src, alt, width, height }: MDXImageProps) {
+  const { openImageModal } = useBlogContext() as BlogContextType;
+
   const url = new URL(src, 'https://poodlepoodle.me');
   const widthParam = url.searchParams.get('width');
   const imageWidth = widthParam ? parseInt(widthParam) : width;
   const isGif = url.pathname.toLowerCase().endsWith('.gif');
+
+  const handleImageClick = () => {
+    openImageModal({ src, alt });
+  };
 
   const baseProps = {
     src,
     alt,
     className: styles.mdx__image,
     ...(isGif && { unoptimized: true }),
+    onClick: handleImageClick,
   } as const;
 
   if (imageWidth) {
