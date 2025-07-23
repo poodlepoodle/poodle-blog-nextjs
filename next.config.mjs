8e816b5d093d28b141d143e4c8fcc3773052c160
poodlepoodle <chammal97@naver.com>
2025-07-24 00:41:21 +0900

    :mag: :: add config related to redirecting old pages

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  reactStrictMode: true,
-};
-
-export default nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: '/posts',
+        permanent: true,
+      },
+      {
+        source: '/blog/:slug*',
+        destination: '/posts/:slug*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
