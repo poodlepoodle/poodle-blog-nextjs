434c3032de5ece242f2ecbac25bd364900315d0e
poodlepoodle <chammal97@naver.com>
2025-11-18 15:31:39 +0900

    :sparkles: :: add new ImageWithSkeleton component

---
+++ src/components/image-with-skeleton/ImageWithSkeleton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Skeleton } from '@components/common/skeleton';
+import { useState } from 'react';
+import { ResponsiveImage } from '@components/common/responsive-image';
+
+type ImageWithSkeletonProps = {
+  src: string;
+  alt: string;
+};
+
+export const ImageWithSkeleton = ({ src, alt }: ImageWithSkeletonProps) => {
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  return (
+    <>
+      {!loaded && <Skeleton fill borderRadius={8} />}
+      <ResponsiveImage
+        src={src}
+        alt={alt}
+        onLoad={() => setLoaded(true)}
+        className={`overflow-hidden rounded-lg object-cover shadow-natural transition-opacity duration-500 ease-in-out ${loaded ? 'opacity-100' : 'opacity-0'}`}
+      />
+    </>
+  );
+};
