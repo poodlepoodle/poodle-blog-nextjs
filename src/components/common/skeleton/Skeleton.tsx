cb2e5e67506781497bdcb96b66000e3f0b0f3e56
poodlepoodle <chammal97@naver.com>
2025-11-18 14:25:33 +0900

    :sparkles: :: add new Skeleton component

---
+++ src/components/common/skeleton/Skeleton.tsx
@@ -0,0 +1,39 @@
+type SkeletonBorderRadius = 0 | 4 | 8 | 16;
+
+type SkeletonProps = {
+  width?: string;
+  height?: string;
+  fill?: boolean;
+  borderRadius?: SkeletonBorderRadius;
+};
+
+const borderRadiusToTailwind: Record<SkeletonBorderRadius, string> = {
+  0: 'rounded-none',
+  4: 'rounded',
+  8: 'rounded-lg',
+  16: 'rounded-2xl',
+};
+
+export const Skeleton = ({
+  width = '100px',
+  height = '100px',
+  fill = false,
+  borderRadius = 0,
+}: SkeletonProps) => {
+  return (
+    <div
+      className={`relative overflow-hidden bg-gray-1 before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-to-r before:from-gray-1 before:via-white before:to-gray-1 ${borderRadiusToTailwind[borderRadius]}`}
+      style={
+        fill
+          ? {
+              width: '100%',
+              height: '100%',
+            }
+          : {
+              width: width,
+              height: height,
+            }
+      }
+    ></div>
+  );
+};
