5f9522000621759d82f7e23a82689c71d608f546
poodlepoodle <chammal97@naver.com>
2025-12-29 00:56:09 +0900

    :label: :: add 'noShadow' optional props

--- src/components/image-with-skeleton/ImageWithSkeleton.tsx
+++ src/components/image-with-skeleton/ImageWithSkeleton.tsx
@@ -7,9 +7,14 @@ import { ResponsiveImage } from '@components/common/responsive-image';
 type ImageWithSkeletonProps = {
   src: string;
   alt: string;
+  noShadow?: boolean;
 };
 
-export const ImageWithSkeleton = ({ src, alt }: ImageWithSkeletonProps) => {
+export const ImageWithSkeleton = ({
+  src,
+  alt,
+  noShadow = false,
+}: ImageWithSkeletonProps) => {
   const [loaded, setLoaded] = useState<boolean>(false);
 
   return (
@@ -19,7 +24,7 @@ export const ImageWithSkeleton = ({ src, alt }: ImageWithSkeletonProps) => {
         src={src}
         alt={alt}
         onLoad={() => setLoaded(true)}
-        className={`overflow-hidden rounded-lg object-cover shadow-natural transition-opacity duration-500 ease-in-out ${loaded ? 'opacity-100' : 'opacity-0'}`}
+        className={`overflow-hidden rounded-lg object-cover transition-opacity duration-500 ease-in-out ${loaded ? 'opacity-100' : 'opacity-0'} ${noShadow ? '' : 'shadow-natural'}`}
       />
     </>
   );
