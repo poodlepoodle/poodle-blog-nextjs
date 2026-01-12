a21187b1f71f4857ddcd6c84fdd949d31f90949a
poodlepoodle <chammal97@naver.com>
2026-01-12 19:36:38 +0900

    :bug: :: modify conditions to distinguish MDXInlineCode to MDXCode

--- src/components/mdx/mdx-components/MDXCode.tsx
+++ src/components/mdx/mdx-components/MDXCode.tsx
@@ -9,12 +9,37 @@ type MDXCodeProps = {
 };
 
 export const MDXCode = ({ children, ...props }: MDXCodeProps) => {
+  let codeContent = children;
+  let lang: string | undefined = undefined;
+
+  if (
+    children &&
+    typeof children === 'object' &&
+    'props' in children &&
+    children.props
+  ) {
+    const childProps = children.props as {
+      children?: React.ReactNode;
+      className?: string;
+    };
+
+    codeContent = childProps.children;
+
+    if (childProps.className) {
+      const match = childProps.className.match(/language-(\w+)/);
+      if (match) {
+        lang = match[1];
+      }
+    }
+  }
+
   return (
     <Code
       {...props}
+      lang={lang}
       className="rounded-lg border-[0.5px] border-gray-2 text-base tablet:text-lg"
     >
-      {children}
+      {codeContent}
     </Code>
   );
 };
