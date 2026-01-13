202dada36e1ad24695435efdaa2e6d6c3496ad8d
poodlepoodle <chammal97@naver.com>
2026-01-13 09:36:02 +0900

    :label: :: replace any props type with appropriate MDXInlineCodeProps

--- src/components/mdx/mdx-components/MDXInlineCode.tsx
+++ src/components/mdx/mdx-components/MDXInlineCode.tsx
@@ -1,4 +1,10 @@
-export const MDXInlineCode = (props: any) => {
+type MDXInlineCodeProps = {
+  children: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+} & React.HTMLAttributes<HTMLElement>;
+
+export const MDXInlineCode = (props: MDXInlineCodeProps) => {
   if (!props) {
     return <code />;
   }
@@ -15,7 +21,7 @@ export const MDXInlineCode = (props: any) => {
 
   return (
     <code
-      className="mx-[0.1rem] inline-block translate-y-[-0.1rem] rounded-lg border-[0.5px] border-[#BDE4F9] bg-[#EFF9FF] px-[0.35rem] py-[0.1rem] align-middle !font-mono !text-xs leading-[inherit] font-medium text-[#176997] before:content-[''] after:content-[''] tablet:py-[0.15rem] tablet:!text-sm"
+      className="mx-[0.1rem] inline-block translate-y-[-0.08rem] rounded-lg border-[0.5px] border-[#BDE4F9] bg-[#EFF9FF] px-[0.35rem] py-[0.1rem] align-middle !font-mono !text-xs leading-[inherit] font-medium text-[#176997] before:content-[''] after:content-[''] tablet:py-[0.15rem] tablet:!text-sm"
       {...rest}
     >
       {children}
