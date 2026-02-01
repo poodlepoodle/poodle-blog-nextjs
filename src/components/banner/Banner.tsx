2c3c52bb93da52e1b5c03b7db08689b5e861d3f1
poodlepoodle <chammal97@naver.com>
2026-02-01 19:18:23 +0900

    :bug: :: fix color-reveal mouse tracking effect bugs

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -7,23 +7,33 @@ import { ImageWithSkeleton } from '@components/image-with-skeleton';
 import { cn } from '@/utils/cn';
 
 export const Banner = () => {
-  const { containerRef } = useGrayscaleReveal<HTMLAnchorElement>();
+  const { containerRef } = useGrayscaleReveal<HTMLDivElement>();
 
   return (
     <Link
       href={`/about`}
-      ref={containerRef}
       className={cn(
-        'group relative h-[6.25rem] w-full shrink-0 overflow-hidden rounded-lg grayscale-100 transition-all duration-300 hover:grayscale-0',
+        'group relative h-[6.25rem] w-full shrink-0 overflow-hidden rounded-lg',
         styles.container
       )}
     >
-      <ImageWithSkeleton
-        src={`/components/banner/banner-background.jpg`}
-        alt="blog home banner image"
-        className="border-none"
-      />
-      <div className="absolute inset-0 z-2 flex items-center justify-center px-[2.5rem]">
+      <div ref={containerRef} className={styles.image__container}>
+        <div className={styles.grayscale__layer}>
+          <ImageWithSkeleton
+            src={`/components/banner/banner-background.jpg`}
+            alt="blog home banner image grayscale"
+            className="border-none"
+          />
+        </div>
+        <div className={styles.color__layer}>
+          <ImageWithSkeleton
+            src={`/components/banner/banner-background.jpg`}
+            alt="blog home banner image"
+            className="border-none"
+          />
+        </div>
+      </div>
+      <div className="absolute inset-0 z-10 flex items-center justify-center px-[2.5rem]">
         <span className="text-center text-sm font-medium break-keep text-gray-3 transition-colors duration-300 group-hover:text-skyblue">
           애정을 담아 사용자와 인터랙션하고 싶은 프론트엔드 개발자 최어진입니다.
         </span>
