20213ff29239a45e86f7c050575bcb59eece23ce
poodlepoodle <chammal97@naver.com>
2026-02-14 22:22:37 +0900

    :recycle: :: move postTitle and activeHeading states into post-store

--- src/stores/post-store.ts
+++ src/stores/post-store.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 
 /**
+ * postTitle: 현재 화면에 보이는 포스트의 제목
+ * activeHeading: 현재 화면 상에 보이는 부분의 헤딩 정보
  * isImageModalOpen: 포스트 내 MDX Image를 확대해서 보여주는 모달 상태
  * imageModalData: 모달에 표시할 이미지 데이터
  */
@@ -11,11 +13,15 @@ export type PostImage = {
 };
 
 type PostState = {
+  postTitle: string;
+  activeHeading: string[];
   isImageModalOpen: boolean;
   imageModalData: PostImage | null;
 };
 
 type PostActions = {
+  setPostTitle: (postTitle: string) => void;
+  setActiveHeading: (activeHeading: string[]) => void;
   openImageModal: () => void;
   setImageModalData: (imageModalData: PostImage) => void;
   closeImageModal: () => void;
@@ -24,12 +30,18 @@ type PostActions = {
 export type PostStore = PostState & PostActions;
 
 const initialState: PostState = {
+  postTitle: '',
+  activeHeading: [],
   isImageModalOpen: false,
   imageModalData: null,
 };
 
 export const usePostStore = create<PostStore>(set => ({
   ...initialState,
+
+  setPostTitle: postTitle => set({ postTitle }),
+
+  setActiveHeading: activeHeading => set({ activeHeading }),
 
   openImageModal: () => set({ isImageModalOpen: true }),
 
--- src/stores/ui-store.ts
+++ src/stores/ui-store.ts
@@ -2,19 +2,16 @@ import { create } from 'zustand';
 
 /**
  * isFloating: Header 컴포넌트의 페이지 상단 고정 여부
- * headerText: Header 컴포넌트에서 표시하는 텍스트
  * isSpotlighted: 포스트 내 Spotlight 효과 적용 여부
  */
 
 type UIState = {
   isFloating: boolean;
-  headerText: string;
   isSpotlighted: boolean;
 };
 
 type UIActions = {
   setIsFloating: (isFloating: boolean) => void;
-  setHeaderText: (headerText: string) => void;
   setIsSpotlighted: (isSpotlighted: boolean) => void;
 };
 
@@ -22,7 +19,6 @@ export type UIStore = UIState & UIActions;
 
 const initialState: UIState = {
   isFloating: false,
-  headerText: '',
   isSpotlighted: false,
 };
 
@@ -30,8 +26,6 @@ export const useUIStore = create<UIStore>(set => ({
   ...initialState,
 
   setIsFloating: isFloating => set({ isFloating }),
-
-  setHeaderText: headerText => set({ headerText }),
 
   setIsSpotlighted: isSpotlighted => set({ isSpotlighted }),
 }));
