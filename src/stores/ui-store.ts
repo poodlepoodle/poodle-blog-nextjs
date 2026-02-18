e12bbb3927f3fc2f16f53676fac4057581105c5f
poodlepoodle <chammal97@naver.com>
2026-02-18 17:08:01 +0900

    :sparkles: :: add Drawer states and actions

--- src/stores/ui-store.ts
+++ src/stores/ui-store.ts
@@ -3,16 +3,20 @@ import { create } from 'zustand';
 /**
  * isFloating: Header 컴포넌트의 페이지 상단 고정 여부
  * isSpotlighted: 포스트 내 Spotlight 효과 적용 여부
+ * isDrawerOpen: 모바일 헤더 드로어 열림 여부
  */
 
 type UIState = {
   isFloating: boolean;
   isSpotlighted: boolean;
+  isDrawerOpen: boolean;
 };
 
 type UIActions = {
   setIsFloating: (isFloating: boolean) => void;
   setIsSpotlighted: (isSpotlighted: boolean) => void;
+  openDrawer: () => void;
+  closeDrawer: () => void;
 };
 
 export type UIStore = UIState & UIActions;
@@ -20,6 +24,7 @@ export type UIStore = UIState & UIActions;
 const initialState: UIState = {
   isFloating: false,
   isSpotlighted: false,
+  isDrawerOpen: false,
 };
 
 export const useUIStore = create<UIStore>(set => ({
@@ -28,4 +33,8 @@ export const useUIStore = create<UIStore>(set => ({
   setIsFloating: isFloating => set({ isFloating }),
 
   setIsSpotlighted: isSpotlighted => set({ isSpotlighted }),
+
+  openDrawer: () => set({ isDrawerOpen: true }),
+
+  closeDrawer: () => set({ isDrawerOpen: false }),
 }));
