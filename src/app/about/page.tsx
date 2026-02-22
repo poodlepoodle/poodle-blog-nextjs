1c52b0ea4a01bbacfe4e49a4f914e8db38e2d24a
poodlepoodle <chammal97@naver.com>
2026-02-22 19:37:43 +0900

    :fire: :: remove profile picture assets and codes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 
 import Link from 'next/link';
-import Image from 'next/image';
 import { Paper } from '@components/common/paper';
 import {
   METADATA_PRESET,
@@ -20,7 +19,7 @@ export const metadata: Metadata = {
     url: '/about',
     images: [
       {
-        url: '/about/introduction-1.jpg',
+        url: '/og/og-large.jpg',
         alt: 'poodle blog profile large image',
       },
     ],
@@ -47,7 +46,7 @@ const ArrowIcon = () => {
 export default async function Page() {
   return (
     <section className="flex w-full flex-col items-center">
-      <Paper className="mt-container-top mb-container-bottom flex h-full w-full flex-col gap-items py-[3.75rem]">
+      <Paper className="mt-container-top mb-container-bottom flex h-full w-full flex-col gap-[5rem] py-[3.75rem]">
         <section className="flex w-full flex-col items-end gap-[2rem] px-[3.75rem]">
           <h1 className="w-fit text-right text-lg font-semibold break-keep text-black transition-transform duration-350 ease-in-out hover:-translate-y-2">
             안녕하세요.
@@ -58,14 +57,6 @@ export default async function Page() {
             프론트엔드 개발자 최어진입니다.
           </h2>
         </section>
-        <div className="relative h-[20rem] w-full overflow-hidden rounded-2xl mix-blend-luminosity shadow-natural">
-          <Image
-            src="/about/introduction-1.jpg"
-            alt="introduction image 1"
-            fill
-            className="object-cover"
-          />
-        </div>
         <div className="flex w-full justify-end gap-items px-[3.75rem]">
           {ABOUT_LINK_ITEMS.map(({ href, label }) => (
             <Link
